0251177acf6b3b0487713d48b6028a306fa5f214
Felix Grabler <grabler@student.tugraz.at>
2026-02-04 22:16:50 +0100

    imp, pubilc telegram

--- distributor/src/App.vue
+++ distributor/src/App.vue
@@ -31,9 +31,8 @@
         <h4>Impressum</h4>
         <p><b>Informationen und Offenlegung gemäß &sect;5 (1) ECG, &sect; 25 MedienG, &sect; 63 GewO und &sect; 14 UGB</b></p>
         <p><b>Webseitenbetreiber:</b> Felix Grabler</p>
-        <p><b>Anschrift:</b> -, 8020 Graz</p>
-        <p><b>UID-Nr:</b> <br /> <b>Gewerbeaufsichtbehörde:</b> <br /> <b>Mitgliedschaften:</b></p>
-        <p><b>Kontaktdaten:</b> <br /> Telefon: <br /> Email: info@grabler.me <br /> Fax:</p>
+        <p><b>Anschrift:</b>8020 Graz</p>
+        <p><b>Kontaktdaten:</b> <br /> Email: info@grabler.me <br /></p>
         <p><b>Anwendbare Rechtsvorschrift:</b> www.ris.bka.gv.at <br /> <b>Berufsbezeichnung:</b></p>
         <p><b>Online Streitbeilegung:</b> Verbraucher, welche in Österreich oder in einem sonstigen Vertragsstaat der ODR-VO niedergelassen sind, haben die Möglichkeit Probleme bezüglich dem entgeltlichen Kauf von Waren oder Dienstleistungen im Rahmen einer Online-Streitbeilegung (nach OS, AStG) zu lösen. Die Europäische Kommission stellt eine Plattform hierfür bereit: https://ec.europa.eu/consumers/odr</p>
         <p><b>Urheberrecht:</b> Die Inhalte dieser Webseite unterliegen, soweit dies rechtlich möglich ist, diversen Schutzrechten (z.B dem Urheberrecht). Jegliche Verwendung/Verbreitung von bereitgestelltem Material, welche urheberrechtlich untersagt ist, bedarf schriftlicher Zustimmung des Webseitenbetreibers.</p>
